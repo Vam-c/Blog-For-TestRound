f68754bdb7c81acbc7ecbfae7d215194b6751dd5
vamsee <vamsee.somasi@gmail.com>
2022-02-05 21:30:10 +0530

    Add get and post methods to fetch posts. Add database connectivity through mongod

--- app.js
+++ app.js
@@ -5,8 +5,38 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/testblogdb",{useNewUrlParser: true});
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+const Post = new mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res){
-    res.send("This is the response");
+    Post.find({}, function(err, foundPosts){
+        if(err){
+            res.send(err);
+        } else {
+            res.send(foundPosts);
+        }
+    });
+
+});
+
+//API For creating posts (user sends title and content)
+app.post("/create", function(req, res){
+    const newPost = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newPost.save(function(err){
+        if(err){
+            res.send(err);
+        } else {
+            res.send("Post successfully saved");
+        }
+    });
 });
 
 app.listen(3000, function() {
