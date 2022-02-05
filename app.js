35884d292af8d2aabef0ca183a3b7ec533058782
vamsee <vamsee.somasi@gmail.com>
2022-02-05 23:41:50 +0530

    Add delete, update and get requests for individual posts

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.get("/", function(req, res){
             res.send(foundPosts);
         }
     });
-
 });
 
 //API For creating posts (user sends title and content)
@@ -37,6 +36,41 @@ app.post("/create", function(req, res){
             res.send("Post successfully saved");
         }
     });
+});
+
+//API for fetching particular post
+app.route("/:postName")
+.get(function(req, res){
+    Post.findOne({title: req.params.postName}, function(err, foundPost){
+        if(err){
+            res.send(err);
+        } else if(foundPost) {
+            res.send(foundPost);
+        } else {
+            res.send("Post does not exist");
+        }
+    });
+})
+.delete(function(req, res){
+    Post.deleteOne({title: req.params.postName}, function(err){
+        if(err){
+            res.send(err);
+        } else {
+            res.send("Item successfully deleted");
+        }
+    });
+})
+.put(function(req, res){
+    Post.findOneAndUpdate({title: req.params.postName},
+        {$set: {title: req.body.title, content: req.body.content}},
+        function(err){
+            if(err){
+                res.send(err);
+            } else {
+                res.send("Item successfully updated");
+            }
+        }
+    );
 });
 
 app.listen(3000, function() {
